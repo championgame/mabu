8067d241335f38df0f48b712dd7ac939ee87b4e0
championgame <81562635+championgame@users.noreply.github.com>
2021-03-30 10:34:44 +0800

    Create info.js

---
+++ info.js
@@ -0,0 +1,20 @@
+const infoList = [
+  {
+    account: 'aqdemo1',
+    token:
+
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZW1iZXJJRCI6ImFxZGVtbzFAYXFkZW1vLnNhbGVzYWNjIiwiZXhwIjoxNjE2ODA5NzM4MzE3LCJpYXQiOjE2MTY3MjMzMzgzMTd9.gsx6GNIeUoYbxIgy7_rrYBe2l-Q6mjityB0LHyaZ1VU',
+},
+{
+    account: 'aqdemo2',
+    token:
+
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZW1iZXJJRCI6ImFxZGVtbzJAYXFkZW1vLnNhbGVzYWNjIiwiZXhwIjoxNjE2ODA5NzM4ODE0LCJpYXQiOjE2MTY3MjMzMzg4MTR9.iqL5e7S89_Ke-Vta_Gb1mKimB_upr8-UlVxcwoQ9bpo',
+},
+{
+    account: 'aqdemo3',
+    token:
+
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZW1iZXJJRCI6ImFxZGVtbzNAYXFkZW1vLnNhbGVzYWNjIiwiZXhwIjoxNjE2ODA5NzM5MzE3LCJpYXQiOjE2MTY3MjMzMzkzMTd9.FBbfOkP0BXnQGNgRaBtIXLg6jIZa5QgrIgHliXW3isQ',
+},
+]
